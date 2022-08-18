495802ae146615e3e83f26dffa9300728965d4db
wucood <wucood@gmail.com>
2022-08-18 11:00:41 +0800

    feat: add scripts

---
+++ scripts/init_centos7.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+# Author: eleven9817@gmail.com
+# Url: https://github.com/wuhaomiao/auto_install
+
+
+
+# 初始化CentOS
+
+# 如果已初始化, 则跳过
+[ ! -s /etc/profile.d/wuhaomiao.sh ] && echo "检测到已初始化, 想重新初始化, 请删除文件: /etc/profile.d/wuhaomiao.sh" && exit
+
+# 关闭selinux
+[ $(getenforce) != "Disabled" ] && setenforce 0 && sed -i 's/^SELINUX=.*$/SELINUX=disabled/' /etc/selinux/config
+
+# 设置字符集
+[ $(echo $LANG) != "en_US.UTF-8" ] && echo LANG="en_US.UTF-8" > /etc/locale.conf && source /etc/locale.conf
+
+# 安装必备软件
+yum -y install redhat-lsb-core ntpdate wget
+
+
+# 自定义环境变量
+cat > /etc/profile.d/wuhaomiao.sh << EOF
+HISTSIZE=10000
+PS1="\[\e[37;40m\][\[\e[32;40m\]\u\[\e[37;40m\]@\h \[\e[35;40m\]\W\[\e[0m\]]\\\\$ "
+HISTTIMEFORMAT="%F %T \$(whoami) "
+
+alias l='ls -AFhlt'
+alias lh='l | head'
+alias vi=vim
+
+GREP_OPTIONS="--color=auto"
+alias grep='grep --color'
+alias egrep='egrep --color'
+alias fgrep='fgrep --color'
+EOF
+
+# 优化Limit
+[ -e /etc/security/limits.d/*nproc.conf ] && rename nproc.conf nproc.conf_bk /etc/security/limits.d/*nproc.conf
+sed -i '/^# End of file/,$d' /etc/security/limits.conf
+cat >> /etc/security/limits.conf <<EOF
+# End of file
+* soft nproc 1000000
+* hard nproc 1000000
+* soft nofile 1000000
+* hard nofile 1000000
+EOF
+
+# 设置时区
+rm -fr /etc/localtime
+ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+# 设置时间同步定时任务
+ntpdate -u pool.ntp.org || ntpdate -u ntp.aliyun.com
+[ ! -e "/var/spool/cron/root" -o -z "$(grep 'ntpdate' /var/spool/cron/root)" ] && { echo "*/20 * * * * $(which ntpdate) -u pool.ntp.org > /dev/null 2>&1" >> /var/spool/cron/root;chmod 600 /var/spool/cron/root; }
+# 连接跟踪调优
+[ ! -e "/etc/sysconfig/modules/iptables.modules" ] && { echo -e "modprobe nf_conntrack\nmodprobe nf_conntrack_ipv4" > /etc/sysconfig/modules/iptables.modules; chmod +x /etc/sysconfig/modules/iptables.modules; }
+modprobe nf_conntrack
+modprobe nf_conntrack_ipv4
+echo options nf_conntrack hashsize=131072 > /etc/modprobe.d/nf_conntrack.conf
+
+# /etc/sysctl.conf
+[ ! -e "/etc/sysctl.conf_bk" ] && /bin/mv /etc/sysctl.conf{,_bk}
+cat > /etc/sysctl.conf << EOF
+fs.file-max=1000000
+net.ipv4.tcp_max_tw_buckets = 6000
+net.ipv4.tcp_sack = 1
+net.ipv4.tcp_window_scaling = 1
+net.ipv4.tcp_rmem = 4096 87380 4194304
+net.ipv4.tcp_wmem = 4096 16384 4194304
+net.ipv4.tcp_max_syn_backlog = 16384
+net.core.netdev_max_backlog = 32768
+net.core.somaxconn = 32768
+net.core.wmem_default = 8388608
+net.core.rmem_default = 8388608
+net.core.rmem_max = 16777216
+net.core.wmem_max = 16777216
+net.ipv4.tcp_timestamps = 1
+net.ipv4.tcp_fin_timeout = 20
+net.ipv4.tcp_synack_retries = 2
+net.ipv4.tcp_syn_retries = 2
+net.ipv4.tcp_syncookies = 1
+#net.ipv4.tcp_tw_len = 1
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_mem = 94500000 915000000 927000000
+net.ipv4.tcp_max_orphans = 3276800
+net.ipv4.ip_local_port_range = 1024 65000
+net.nf_conntrack_max = 6553500
+net.netfilter.nf_conntrack_max = 6553500
+net.netfilter.nf_conntrack_tcp_timeout_close_wait = 60
+net.netfilter.nf_conntrack_tcp_timeout_fin_wait = 120
+net.netfilter.nf_conntrack_tcp_timeout_time_wait = 120
+net.netfilter.nf_conntrack_tcp_timeout_established = 3600
+EOF
+sysctl -p
+
+source /etc/profile
+
+
